9754f17761acd08a9091de4a421c3b5e1f1f4347
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-04-23 16:20:01 +0200

    fixed some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,25 @@
 import { getImages } from "~/server/query";
-
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images  = await getImages();
+  const images = await getImages();
   return (
     <main className="mt-4">
-      <div className="flex flex-wrap gap-4 h-[100px]">
-        
+      <div className="flex h-[100px] flex-wrap gap-4 justify-center">
         {images.map((image) => (
-          <img
-            key={image.id}
-            src={image.url}
-            alt="Image"
-            height={100}
-            className="object-cover h-[100px] w-auto"
-          />
+          <div key={image.id} className="flex flex-col">
+            <Image
+              src={image.url}
+              alt="Image"
+              style={{ objectFit: "contain" }}
+              width={480}
+              height={100}
+              className="h-[100px] w-auto object-cover"
+            />
+            <div>{image.name}</div>
+          </div>
         ))}
       </div>
     </main>

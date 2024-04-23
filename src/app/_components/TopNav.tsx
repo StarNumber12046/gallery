643861f178476112036c05453bf2066f828ffc95
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-04-23 21:12:51 +0200

    Added shadui

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,20 +1,19 @@
 "use client";
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { UploadButton } from "~/app/_components/UploadButton";
 
 
 export function TopNav() {
-  const router = useRouter();
   return (
     <nav className="flex w-screen justify-between border-b border-b-neutral-600 bg-black p-4 font-semibold relative text-white">
       <div>Gallery</div>
-      <div className="inline-flex">
+      <div className="inline-flex gap-4 flex">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton endpoint="imageUploader" onClientUploadComplete={() => router.refresh()} />
+          <UploadButton />
           <UserButton />
         </SignedIn>
       </div>
